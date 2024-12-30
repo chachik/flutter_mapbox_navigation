ec6a5966c6c6c2583300d6782d67e2486e4a5243
Oleksandr Nechyporenko <alexander.nechyporenko@gmail.com>
2024-12-30 18:04:41 +0200

    The fix of the following bug: FormatException: Unexpected character (at character 56) {  "eventType": "banner_instruction",  "data": "Центр "Зустріч культур" зна...

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/utilities/PluginUtilities.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/utilities/PluginUtilities.kt
@@ -35,7 +35,7 @@ class PluginUtilities {
 
         fun sendEvent(event: MapBoxRouteProgressEvent) {
             val dataString = event.toJson()
-            val jsonString = "{" +
+            val jsonString = "{" +  
                     "  \"eventType\": \"${MapBoxEvents.PROGRESS_CHANGE.value}\"," +
                     "  \"data\": $dataString" +
                     "}"
@@ -43,13 +43,14 @@ class PluginUtilities {
         }
 
         fun sendEvent(event: MapBoxEvents, data: String = "") {
+            var clean_data = data.replace("\"", "'")
             val jsonString =
                 if ((MapBoxEvents.MILESTONE_EVENT == event || event == MapBoxEvents.USER_OFF_ROUTE || event == MapBoxEvents.ROUTE_BUILT || event == MapBoxEvents.ON_MAP_TAP) && data != "") "{" +
                         "  \"eventType\": \"${event.value}\"," +
                         "  \"data\": $data" +
                         "}" else "{" +
                         "  \"eventType\": \"${event.value}\"," +
-                        "  \"data\": \"$data\"" +
+                        "  \"data\": \"$clean_data\"" +
                         "}"
             FlutterMapboxNavigationPlugin.eventSink?.success(jsonString)
         }
